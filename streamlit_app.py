33b415a5a12407cf2634c4f37dc2cafd34ae3ea8
emm-ac <emmanuel.ac6@gmail.com>
2022-11-25 17:27:42 -0300

    cambio conexión

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,19 @@ import psycopg2
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+#@st.experimental_singleton
+#def init_connection():
+#    return psycopg2.connect(**st.secrets["postgres"])
+#
+#conn = init_connection()
 
-conn = init_connection()
+
+# create database connection
+@st.cache(allow_output_mutation=True)
+def get_database_connection():
+    conn = sqlite3.connect('sup_db')
+    c = conn.cursor()   
+    return c
 
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
