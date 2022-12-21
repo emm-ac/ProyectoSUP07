e5af56cc5a6eee2238ce5296910b66dfdf0a1139
emm-ac <emmanuel.ac6@gmail.com>
2022-12-20 21:38:27 -0300

    Actualizar Juegos

--- pages/3 Juegos.py
+++ pages/3 Juegos.py
@@ -5,3 +5,12 @@ st.set_page_config(page_title='Juegos',
                    layout="centered", 
                    initial_sidebar_state="collapsed", 
                    menu_items=None)
+
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
