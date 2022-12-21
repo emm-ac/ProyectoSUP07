39380c6a75b884a6fe092309b7cc124ecc2123a1
emm-ac <emmanuel.ac6@gmail.com>
2022-12-20 21:32:45 -0300

    Cambios en código

--- Prueba Postgres _ Server.sql
+++ Prueba Postgres _ Server.sql
@@ -1,15 +1,19 @@
-drop if exists Alumnos;
+drop table Alumnos;
 create table Alumnos(
 IDAlumno	int not null,
 Nombre_apellido varchar(100),
+Edad int not null,
+País varchar(50) not null,
 Carrera varchar(5) not null,
 Cohorte	int not null,
-Mail varchar(100),
+Dispositivo_usado varchar(100),
+Cam_Mic varchar(50),
+Actividad_preferida varchar(100),
 primary key (IDAlumno)
 );
 
-insert into Alumnos values(0,'Emmanuel Corral','DS','05','emmanuel.ac@hotmail.com');
-insert into Alumnos values(1,'Melina Griffo','DS','05','meli.griffo@hotmail.com');
-insert into Alumnos values(2,'Federico Olivieri','DS','03','fede.olivieri@hotmail.com');
+insert into Alumnos values(0,'Emmanuel Corral',35,'Argentina','DS',05,'Notebook','Ambos','Juegos');
+insert into Alumnos values(1,'Melina Griffo',30,'Argentina','DS',05,'Notebook','Mic','Charlar');
+insert into Alumnos values(2,'Federico Olivieri',29,'Argentina','DS',03,'Notebook','Ambos','Dar órdenes');
 
 select * from alumnos order by IDAlumno;
--- pages/2 Estadisticas.py
+++ pages/2 Estadisticas.py
@@ -21,19 +21,19 @@ def init_connection():
 conn = init_connection()
 
 
-## Perform query.
-## Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-#@st.experimental_memo(ttl=600)
-#def run_query(query):
-#    with conn.cursor() as cur:
-#        cur.execute(query)
-#        return cur.fetchall()
-#
-#rows = run_query("SELECT * FROM Alumnos")                 
-#
-## Print results.
-#for row in rows:
-#    st.write(f"{row[0]} has a :{row[1]}:")
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * FROM Alumnos")                 
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
     
 
 
