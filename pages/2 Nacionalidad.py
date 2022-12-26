3f902e09dc069de7d419fba129b6418cb561599c
emm-ac <emmanuel.ac6@gmail.com>
2022-12-26 12:14:55 -0300

    Actualizar código Edades

--- pages/2 Nacionalidad.py
+++ pages/2 Nacionalidad.py
@@ -34,12 +34,28 @@ st.subheader('Nacionalidades')
 st.markdown(f'La distribución de nacionalidades es la siguiente:')
 sql2 = pd.DataFrame(run_query("SELECT COUNT(IDAlumno) FROM Alumnos GROUP BY país"))
 
-sql2.to_dataframe()
-
-
 
 st.markdown(sql2)
 #sql2 = sql2(columns=('País','Cantidad'))
 st.write(sql2)
 st.dataframe(data=sql2, use_container_width=True)
 st.bar_chart(data=sql2, use_container_width=True)
+
+
+st.subheader('Edades de los alumnos')
+sql111 = pd.DataFrame(run_query("SELECT Nombre_apellido, edad FROM Alumnos"))
+sql111.columns = ['Nombre','Edad']
+sql111.style.hide_index()
+
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+# Display a static table
+st.table(sql111)
